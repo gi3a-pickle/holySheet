22e66bb043fc6254321f46d5f62fe61b511709eb
gi3a-pickle <gi3a.pickle@gmail.com>
2024-05-26 20:11:14 -0500

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+      SPREAD_SHEET_ID: process.env.SPREAD_SHEET_ID
+    }
+  }
+})
