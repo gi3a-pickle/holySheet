7816c8293dcf1147a54976ef31106f3af97df3bb
gi3a-pickle <gi3a.pickle@gmail.com>
2024-05-27 19:49:39 -0500

    stable version

---
+++ components/UnutHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <img src="@/public/logo.png" />
+    <h3>UNUT SNACKS TRANSPORTATION</h3>
+    <span>2302 Foster Ave, Wheeling, IL 60090, Phone: 800-369-9394</span>
+    <div>
+        <span>Settlement Code Number 19</span>
+        <span>Truck Number 01</span>
+        <span>Driver ID 01</span>
+        <span>461 WINSLOW WAY LAKE IN THE HILLS, IL 60156</span>
+        <span>{{ driverName }}</span>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    driverName: String
+});
+</script>
--- pages/driver/[name].vue
+++ pages/driver/[name].vue
@@ -78,16 +78,7 @@ onMounted(() => {
         </div>
         <button @click="generateReport">Generate Report PDF</button>
         <div id="report-content">
-            <img src="/logo.png" />
-            <h3>UNUT SNACKS TRANSPORTATION</h3>
-            <span>2302 Foster Ave, Wheeling, IL 60090, Phone: 800-369-9394</span>
-            <div>
-                <span>Settlement Code Number 19</span>
-                <span>Truck Number 01</span>
-                <span>Driver ID 01</span>
-                <span>461 WINSLOW WAY LAKE IN THE HILLS, IL 60156</span>
-                <span>{{ driverName }}</span>
-            </div>
+            <UnutHeader :driverName="driverName" />
             <RouteList :headings="headings" :result="filteredRoutes" />
 
         </div>
